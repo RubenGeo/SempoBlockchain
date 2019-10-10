f5309f5822d78fa32f174be7c3737e3a39ffc0a3
RubenGeo <rubenavalk@gmail.com>
2019-10-10 13:12:10 +0200

    Added kobo_api & fields in user table & separate table for custom user attributes
    - Added kobo_api as a seperate end point in which the custom attributes are fist extracted and the standard UserUtils.proccess_create_or_modify_user_request function is used to add the user
    - Created a custom_attribute_user_storage a related table in which custom attributes are stored
    - Added prefered language, bio and gender as a column in the user table
    - Functionality to input values in these columns when creating or updating a new user

--- app/server/api/user_api.py
+++ app/server/api/user_api.py
@@ -20,16 +20,19 @@ class UserAPI(MethodView):
     @requires_auth
     def get(self, user_id):
 
-        can_see_full_details = AccessControl.has_suffient_role(g.user.roles,{'ADMIN': 'admin'})
+        can_see_full_details = AccessControl.has_suffient_role(
+            g.user.roles, {'ADMIN': 'admin'})
 
         if not can_see_full_details:
             public_serial_number = request.args.get('public_serial_number')
 
             if public_serial_number:
-                user = User.query.filter_by(public_serial_number=public_serial_number.strip()).first()
+                user = User.query.filter_by(
+                    public_serial_number=public_serial_number.strip()).first()
 
                 if user:
-                    transfer_account = TransferAccount.query.get(user.transfer_account_id)
+                    transfer_account = TransferAccount.query.get(
+                        user.transfer_account_id)
 
                     if transfer_account:
                         response_object = {
@@ -91,7 +94,8 @@ class UserAPI(MethodView):
                 user_query = User.query.filter(User.has_vendor_role)
 
             elif account_type_filter == 'admin':
-                user_query = User.query.filter(User.has_admin_role).order_by(User.created.desc())
+                user_query = User.query.filter(
+                    User.has_admin_role).order_by(User.created.desc())
 
             else:
                 user_query = User.query
@@ -122,15 +126,6 @@ class UserAPI(MethodView):
 
         post_data = request.get_json()
 
-        # Data supplied to the API via integrations such as KoboToolbox can be messy, so clean the data first
-        dict_processor = AttributeDictProccessor(post_data)
-        dict_processor.force_attribute_dict_keys_to_lowercase()
-        dict_processor.strip_kobo_preslashes()
-        dict_processor.attempt_to_truthy_dict_values()
-        dict_processor.strip_weirdspace_characters()
-        dict_processor.insert_settings_from_databse(CREATE_USER_SETTINGS)
-        post_data = dict_processor.attribute_dict
-
         response_object, response_code = UserUtils.proccess_create_or_modify_user_request(
             post_data,
             organisation=g.user.get_active_organisation()
@@ -141,55 +136,57 @@ class UserAPI(MethodView):
 
         return make_response(jsonify(response_object)), response_code
 
-    @requires_auth(allowed_roles={'ADMIN': 'subadmin'})
-    def put(self, user_id):
-        put_data = request.get_json()
 
-        first_name = put_data.get('first_name')
-        last_name = put_data.get('last_name')
+@requires_auth(allowed_roles={'ADMIN': 'subadmin'})
+def put(self, user_id):
+    put_data = request.get_json()
 
-        email = put_data.get('email')
-        phone = put_data.get('phone')
-        public_serial_number = put_data.get('public_serial_number')
-        location = put_data.get('location')
+    first_name = put_data.get('first_name')
+    last_name = put_data.get('last_name')
 
-        user = User.query.get(user_id)
+    email = put_data.get('email')
+    phone = put_data.get('phone')
+    public_serial_number = put_data.get('public_serial_number')
+    location = put_data.get('location')
 
-        if not user:
-            response_object = {
-                'message': 'User not found'
-            }
-            return make_response(jsonify(response_object)), 400
+    user = User.query.get(user_id)
 
-        if first_name and not first_name == user.first_name:
-            user.first_name = first_name
-
-        if last_name and not last_name == user.last_name:
-            user.last_name = last_name
-
-        if email and not email == user.email:
-            user.email = email
-
-        if phone and not phone == user.phone:
-            user.phone = phone
-
-        if public_serial_number and not public_serial_number == user.public_serial_number:
-            user.public_serial_number = public_serial_number
-
-        if location and not location == user.location:
-            user.location = location
-
-        db.session.commit()
-
+    if not user:
         response_object = {
-            'status': 'success',
-            'message': 'Successfully Edited User.',
-            'data': {
-                'user': user_schema.dump(user).data
-            }
+            'message': 'User not found'
         }
+        return make_response(jsonify(response_object)), 400
 
-        return make_response(jsonify(response_object)), 201
+    if first_name and not first_name == user.first_name:
+        user.first_name = first_name
+
+    if last_name and not last_name == user.last_name:
+        user.last_name = last_name
+
+    if email and not email == user.email:
+        user.email = email
+
+    if phone and not phone == user.phone:
+        user.phone = phone
+
+    if public_serial_number and not public_serial_number == user.public_serial_number:
+        user.public_serial_number = public_serial_number
+
+    if location and not location == user.location:
+        user.location = location
+
+    db.session.commit()
+
+    response_object = {
+        'status': 'success',
+        'message': 'Successfully Edited User.',
+        'data': {
+            'user': user_schema.dump(user).data
+        }
+    }
+
+    return make_response(jsonify(response_object)), 201
+
 
 # add Rules for API Endpoints
 user_blueprint.add_url_rule(
